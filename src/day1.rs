399883e52ee44d9da530bbe87b37faed00fee4d7
GearBoxFox <favoringbasil55@gmail.com>
2024-12-01 14:23:38 -0500

    day 1 finished

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,53 @@
 pub fn solve1(input: &String) -> isize {
+    let lines = input.lines();
+    let mut sum: i32 = 0;
 
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+
+    // get the numbers into the vecs
+    for line in lines {
+        let mut iter = line.split_ascii_whitespace();
+        let num1 = iter.next().unwrap().trim().parse::<i32>().unwrap();
+        let num2 = iter.next().unwrap().trim().parse::<i32>().unwrap();
+
+        left.push(num1);
+        right.push(num2);
+    }
+
+    // sort the vecs from lowest to greatest
+    left.sort();
+    right.sort();
+
+    // assume both vecs are same size
+    for i in 0..left.len() {
+        sum += i32::abs(left.get(i).unwrap() - right.get(i).unwrap())
+    }
+
+    sum as isize
+}
+
+pub fn solve2(input: &String) -> isize {
+    let lines = input.lines();
+    let mut sum: i32 = 0;
+
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+
+    // get the numbers into the vecs
+    for line in lines {
+        let mut iter = line.split_ascii_whitespace();
+        let num1 = iter.next().unwrap().trim().parse::<i32>().unwrap();
+        let num2 = iter.next().unwrap().trim().parse::<i32>().unwrap();
+
+        left.push(num1);
+        right.push(num2);
+    }
+
+    for i in 0..left.len() {
+        let sim = right.iter().filter(|&&x| x == *left.get(i).unwrap()).count();
+        sum += *left.get(i).unwrap() * sim as i32;
+    }
+
+    sum as isize
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,42 @@
+use std::env;
+use std::fs::File;
+use std::io::{Error, ErrorKind, Read};
+use std::path::Path;
+
 mod day1;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Error> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        println!("please choose a day and part");
+        return Err(ErrorKind::Other.into());
+    }
+
+    let day = args[1].parse::<u8>().unwrap_or(1);
+    let part2 = args[2].parse::<bool>().unwrap_or(false);
+    let example = args[3].parse::<bool>().unwrap_or(false);
+
+    let day_path: String;
+
+    if example {
+        day_path = format!("./inputs/day{}example", day);
+    } else {
+        day_path = format!("./inputs/day{}", day);
+    }
+
+    let mut file = File::open(day_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let solution = match day {
+        1 => if !part2 {day1::solve1(&contents)} else {day1::solve2(&contents)}
+        _ => 0
+    };
+
+    println!("Day {}", day);
+    if part2 { println!("Part 2") } else { println!("Part 1") }
+    println!("Solution: {}", solution);
+
+    Ok(())
 }
